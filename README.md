777cd0211fc37c87c664470355618dc4e944b59b
prodigy07 <Luan_henrique21@hotmail.com>
2022-03-14 10:13:40 -0300

    🔗 Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# 🚧 Em Construção!
+
+<hr>
+
+<p>Confira o resultado do projeto em: <a href="https://luanhenriquee.github.io/pokedex-mapadevweek/" target="_blank">luanhenriquee.github.io/pokedex-mapadevweek/</a></p>
+
+<hr>
+
+## 💻 Projeto Pokédex (MapaDevWeek) 
+Neste projeto você irá desenvolver uma pokédex com 6 cards, contendo as informações dos seus respectivos pokemons.
+
+## 🚀 Techs
+<ul>
+  <li> HTML</li>
+  <li> CSS </li>
+  <li> JavaScript </li>
+</ul>
---
+++ src/js/index.js
@@ -0,0 +1,37 @@
+const menuPokemon = document.querySelectorAll('.pokemon');
+const cardPokemon = document.querySelectorAll('.card-pokemon');
+const sound = document.createElement('audio');
+
+const elementCard = cardPokemon[0].firstElementChild;
+const bgCard = window.getComputedStyle(elementCard).backgroundColor;
+menuPokemon[0].style.backgroundColor = bgCard;
+
+function removeActive () {
+    const menuActive = document.querySelector('.active');
+    menuActive.classList.remove('active');
+    menuActive.style.backgroundColor = '#6B727A';
+}
+
+function removeOpen () {
+    const cardOpen = document.querySelector('.open');
+    cardOpen.classList.remove('open');
+}
+
+menuPokemon.forEach((pokemon, index) => {
+    pokemon.addEventListener('click', () => {
+        if (index == 0) {
+            sound.src = "/src/sounds/sound-pikachu.mp3";
+        } else {
+            sound.src = "/src/sounds/sound-button.mp3";
+        }
+        sound.play();
+        removeActive();
+        pokemon.classList.add('active');
+        const elementCard = cardPokemon[index].firstElementChild;
+        const bgCard = window.getComputedStyle(elementCard).backgroundColor;
+        pokemon.style.backgroundColor = bgCard;
+        removeOpen();
+        cardPokemon[index].classList.add('open');
+    });
+});
+
